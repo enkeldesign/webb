29bc90658709ee19ccd2ff71ccc1181c71a1f340
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-03 16:10:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.querySelector('footer input[type="checkbox"]');
-    if (themeToggle) {
-        themeToggle.addEventListener('keydown', function(event) {
-            if (event.key === 'Enter') {
-                this.checked = !this.checked;
-                event.preventDefault();
-            }
-        });
-    }
+  const themeToggle = document.querySelector('footer input[type="checkbox"]');
+  if (themeToggle) {
+    themeToggle.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        this.checked = !this.checked;
+        event.preventDefault();
+      }
+    });
+  }
 });
