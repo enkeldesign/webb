87114d7a1ef116089fd923a6fd5f70fb79adbfe6
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-23 00:36:22 +0100

    Toggle changes UI too

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const themeToggle = document.querySelector('footer input[type="checkbox"]');
+
+  const themeToggle = document.querySelector('input[type="checkbox"]');
+
   if (themeToggle) {
+
+    function updateThemeColor(color) {
+      let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
+      if (!themeColorMetaTag) {
+
+        themeColorMetaTag = document.createElement('meta');
+        themeColorMetaTag.setAttribute('name', 'theme-color');
+        themeColorMetaTag.setAttribute('data-user-theme', 'true');
+        document.head.appendChild(themeColorMetaTag);
+      }
+      themeColorMetaTag.setAttribute('content', color);
+    }
+
+    themeToggle.addEventListener('change', function() {
+      const isDarkMode = this.checked;
+
+      if (isDarkMode) {
+        updateThemeColor('#313131');
+      } else {
+        updateThemeColor('#f4f4f4');
+      }
+    });
+
     themeToggle.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
         this.checked = !this.checked;
         event.preventDefault();
+
+        const changeEvent = new Event('change');
+        this.dispatchEvent(changeEvent);
       }
     });
   }
