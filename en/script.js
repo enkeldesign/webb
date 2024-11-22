078cacc3414bbe843d0778f619e1dfc3a43fd4f7
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-23 00:57:30 +0100

    Create script.js

---
+++ en/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const themeToggle = document.getElementById('theme-toggle');
+  const themeFeedback = document.getElementById('sr-theme-feedback');
+
+  if (themeToggle) {
+
+    function updateThemeColor(color) {
+      let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
+      if (!themeColorMetaTag) {
+
+        themeColorMetaTag = document.createElement('meta');
+        themeColorMetaTag.setAttribute('name', 'theme-color');
+        themeColorMetaTag.setAttribute('data-user-theme', 'true');
+        document.head.appendChild(themeColorMetaTag);
+      }
+      themeColorMetaTag.setAttribute('content', color);
+    }
+
+    themeToggle.addEventListener('change', function() {
+      const isDarkMode = this.checked;
+
+      this.setAttribute('aria-checked', isDarkMode.toString());
+
+      if (isDarkMode) {
+        updateThemeColor('#313131');
+
+        themeFeedback.textContent = 'Dark mode activated.';
+      } else {
+        updateThemeColor('#f4f4f4');
+
+        themeFeedback.textContent = 'Light mode activated.';
+      }
+    });
+
+    themeToggle.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        this.checked = !this.checked;
+        event.preventDefault();
+
+        const changeEvent = new Event('change');
+        this.dispatchEvent(changeEvent);
+      }
+    });
+  }
+});
