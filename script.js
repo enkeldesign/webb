53e4c923ff8aebbcb9371cf84ce9a75d85f29cbe
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-23 01:31:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   const themeToggle = document.getElementById('theme-toggle');
   const themeFeedback = document.getElementById('sr-theme-feedback');
 
   if (themeToggle) {
-
     function updateThemeColor(color) {
       let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
       if (!themeColorMetaTag) {
-
         themeColorMetaTag = document.createElement('meta');
         themeColorMetaTag.setAttribute('name', 'theme-color');
         themeColorMetaTag.setAttribute('data-user-theme', 'true');
@@ -19,25 +16,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
     themeToggle.addEventListener('change', function() {
       const isDarkMode = this.checked;
-
       this.setAttribute('aria-checked', isDarkMode.toString());
+
+      let message = '';
 
       if (isDarkMode) {
         updateThemeColor('#313131');
-
-        themeFeedback.textContent = 'Mörkt färgschema aktiverat.';
+        message = 'Mörkt färgschema aktiverat.';
       } else {
         updateThemeColor('#f4f4f4');
-
-        themeFeedback.textContent = 'Ljust färgschema aktiverat.';
+        message = 'Ljust färgschema aktiverat.';
       }
+
+
+      themeFeedback.setAttribute('lang', 'sv');
+
+      themeFeedback.textContent = message;
     });
 
     themeToggle.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
         this.checked = !this.checked;
         event.preventDefault();
-
         const changeEvent = new Event('change');
         this.dispatchEvent(changeEvent);
       }
