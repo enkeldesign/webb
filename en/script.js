a33cd96be65c17ed200da296a166b9882597c1ef
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-23 21:48:06 +0100

    Update script.js

--- en/script.js
+++ en/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const themeToggle = document.getElementById('theme-toggle');
   const themeFeedback = document.getElementById('sr-theme-feedback');
 
   if (themeToggle) {
+
     function updateThemeColor(color) {
       let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
       if (!themeColorMetaTag) {
@@ -14,22 +16,34 @@ document.addEventListener('DOMContentLoaded', function() {
       themeColorMetaTag.setAttribute('content', color);
     }
 
+
+    function updateThemeFeedback(isDarkMode) {
+      const messages = {
+        dark: {
+          text: 'Dark mode activated.',
+          lang: 'en'
+        },
+        light: {
+          text: 'Light mode activated.',
+          lang: 'en'
+        },
+      };
+
+      const message = isDarkMode ? messages.dark : messages.light;
+      themeFeedback.setAttribute('lang', message.lang);
+      themeFeedback.textContent = message.text;
+    }
+
+
     themeToggle.addEventListener('change', function() {
       const isDarkMode = this.checked;
       this.setAttribute('aria-checked', isDarkMode.toString());
 
-      let message = '';
-      if (isDarkMode) {
-        updateThemeColor('#313131');
-        message = 'Dark mode activated.';
-      } else {
-        updateThemeColor('#f4f4f4');
-        message = 'Light mode activated.';
-      }
 
-      themeFeedback.setAttribute('lang', 'en');
-      themeFeedback.textContent = message;
+      updateThemeColor(isDarkMode ? '#313131' : '#f4f4f4');
+      updateThemeFeedback(isDarkMode);
     });
+
 
     themeToggle.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
@@ -42,11 +56,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+
 function copyEmail() {
-    const email = "erik@enkel.design";
-    navigator.clipboard.writeText(email).then(() => {
+  const email = "erik@enkel.design";
+  navigator.clipboard.writeText(email)
+    .then(() => {
       alert("erik@enkel.design copied to clipboard.");
-    }).catch(err => {
+    })
+    .catch(err => {
       console.error('Error copying text: ', err);
     });
-  }
+}
