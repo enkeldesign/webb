671896ce58bedd2698d0374324626662df47bf7d
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2024-11-23 21:46:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const themeToggle = document.getElementById('theme-toggle');
   const themeFeedback = document.getElementById('sr-theme-feedback');
 
   if (themeToggle) {
+
     function updateThemeColor(color) {
       let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
       if (!themeColorMetaTag) {
@@ -14,24 +16,34 @@ document.addEventListener('DOMContentLoaded', function() {
       themeColorMetaTag.setAttribute('content', color);
     }
 
+
+    function updateThemeFeedback(isDarkMode) {
+      const messages = {
+        dark: {
+          text: 'Mörkt färgschema aktiverat.',
+          lang: 'sv'
+        },
+        light: {
+          text: 'Ljust färgschema aktiverat.',
+          lang: 'sv'
+        },
+      };
+
+      const message = isDarkMode ? messages.dark : messages.light;
+      themeFeedback.setAttribute('lang', message.lang);
+      themeFeedback.textContent = message.text;
+    }
+
+
     themeToggle.addEventListener('change', function() {
       const isDarkMode = this.checked;
       this.setAttribute('aria-checked', isDarkMode.toString());
 
-      let message = '';
 
-      if (isDarkMode) {
-        updateThemeColor('#313131');
-        message = 'Mörkt färgschema aktiverat.';
-      } else {
-        updateThemeColor('#f4f4f4');
-        message = 'Ljust färgschema aktiverat.';
-      }
-
-
-      themeFeedback.setAttribute('lang', 'sv');
-      themeFeedback.textContent = message;
+      updateThemeColor(isDarkMode ? '#313131' : '#f4f4f4');
+      updateThemeFeedback(isDarkMode);
     });
+
 
     themeToggle.addEventListener('keydown', function(event) {
       if (event.key === 'Enter') {
@@ -44,55 +56,14 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const themeToggle = document.getElementById('theme-toggle');
-  const themeFeedback = document.getElementById('sr-theme-feedback');
-
-  if (themeToggle) {
-    function updateThemeColor(color) {
-      let themeColorMetaTag = document.querySelector('meta[name="theme-color"][data-user-theme]');
-      if (!themeColorMetaTag) {
-        themeColorMetaTag = document.createElement('meta');
-        themeColorMetaTag.setAttribute('name', 'theme-color');
-        themeColorMetaTag.setAttribute('data-user-theme', 'true');
-        document.head.appendChild(themeColorMetaTag);
-      }
-      themeColorMetaTag.setAttribute('content', color);
-    }
-
-    themeToggle.addEventListener('change', function() {
-      const isDarkMode = this.checked;
-      this.setAttribute('aria-checked', isDarkMode.toString());
-
-      let message = '';
-      if (isDarkMode) {
-        updateThemeColor('#313131');
-        message = 'Dark mode activated.';
-      } else {
-        updateThemeColor('#f4f4f4');
-        message = 'Light mode activated.';
-      }
-
-      themeFeedback.setAttribute('lang', 'en');
-      themeFeedback.textContent = message;
-    });
-
-    themeToggle.addEventListener('keydown', function(event) {
-      if (event.key === 'Enter') {
-        this.checked = !this.checked;
-        event.preventDefault();
-        const changeEvent = new Event('change');
-        this.dispatchEvent(changeEvent);
-      }
-    });
-  }
-});
 
 function copyEmail() {
-    const email = "erik@enkel.design";
-    navigator.clipboard.writeText(email).then(() => {
+  const email = "erik@enkel.design";
+  navigator.clipboard.writeText(email)
+    .then(() => {
       alert("erik@enkel.design kopierat till urklipp.");
-    }).catch(err => {
+    })
+    .catch(err => {
       console.error('Error copying text: ', err);
     });
-  }
+}
