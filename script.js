1aaebdee5e56418a14c3189b844e1f07318e590d
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2025-03-01 20:31:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
   const themeFeedback = document.getElementById('sr-theme-feedback');
-
   if (!themeToggle) return;
-
   const updateThemeColor = (color) => {
     let themeMeta = document.querySelector('meta[name="theme-color"]');
     if (themeMeta) themeMeta.remove();
-    themeMeta = document.createElement('meta');
-    themeMeta.setAttribute('name', 'theme-color');
-    themeMeta.setAttribute('content', color);
-    document.head.appendChild(themeMeta);
+    requestAnimationFrame(() => {
+      themeMeta = document.createElement('meta');
+      themeMeta.setAttribute('name', 'theme-color');
+      themeMeta.setAttribute('content', color);
+      document.head.appendChild(themeMeta);
+    });
   };
-
   const updateThemeFeedback = (isDark) => {
     const messages = {
       dark: { text: 'Mörkt färgschema aktiverat.', lang: 'sv' },
-      light: { text: 'Ljust färgschema aktiverat.', lang: 'sv' },
+      light: { text: 'Ljust färgschema aktiverat.', lang: 'sv' }
     };
     const { text, lang } = isDark ? messages.dark : messages.light;
     themeFeedback.setAttribute('lang', lang);
     themeFeedback.textContent = text;
   };
-
   const handleThemeToggleChange = (event) => {
     const isDark = event.target.checked;
     event.target.setAttribute('aria-checked', isDark.toString());
     updateThemeColor(isDark ? '#313131' : '#f4f4f4');
     updateThemeFeedback(isDark);
   };
-
   const handleThemeToggleKeydown = (event) => {
     if (event.key === 'Enter') {
       event.target.checked = !event.target.checked;
@@ -37,11 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
       event.target.dispatchEvent(new Event('change'));
     }
   };
-
   themeToggle.addEventListener('change', handleThemeToggleChange);
   themeToggle.addEventListener('keydown', handleThemeToggleKeydown);
 });
-
 const copyEmail = () => {
   const email = "erik@enkel.design";
   navigator.clipboard.writeText(email)
