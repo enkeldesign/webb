97321a82742304d22fbc96ad48b7412fa114654b
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2025-03-02 10:35:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
   const themeToggle = document.getElementById('theme-toggle');
   const themeFeedback = document.getElementById('sr-theme-feedback');
+
   if (!themeToggle) return;
+
   const updateThemeColor = (color) => {
-    let themeMeta = document.querySelector('meta[name="theme-color"]');
-    if (themeMeta) themeMeta.remove();
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        const newMeta = document.createElement('meta');
-        newMeta.setAttribute('name', 'theme-color');
-        newMeta.setAttribute('content', color);
-        document.head.insertBefore(newMeta, document.head.firstChild);
-      });
-    });
+    let themeMeta = document.querySelector('meta[name="theme-color"][data-user-theme]');
+    if (!themeMeta) {
+      themeMeta = document.createElement('meta');
+      themeMeta.setAttribute('name', 'theme-color');
+      themeMeta.setAttribute('data-user-theme', 'true');
+      document.head.appendChild(themeMeta);
+    }
+    themeMeta.setAttribute('content', color);
   };
+
   const updateThemeFeedback = (isDark) => {
     const messages = {
-      dark: { text: 'Mörkt färgschema aktiverat.', lang: 'sv' },
-      light: { text: 'Ljust färgschema aktiverat.', lang: 'sv' }
+      dark: {
+        text: 'Mörkt färgschema aktiverat.',
+        lang: 'sv',
+      },
+      light: {
+        text: 'Ljust färgschema aktiverat.',
+        lang: 'sv',
+      },
     };
+
     const { text, lang } = isDark ? messages.dark : messages.light;
     themeFeedback.setAttribute('lang', lang);
     themeFeedback.textContent = text;
   };
+
   const handleThemeToggleChange = (event) => {
     const isDark = event.target.checked;
     event.target.setAttribute('aria-checked', isDark.toString());
     updateThemeColor(isDark ? '#313131' : '#f4f4f4');
     updateThemeFeedback(isDark);
   };
+
   const handleThemeToggleKeydown = (event) => {
     if (event.key === 'Enter') {
       event.target.checked = !event.target.checked;
@@ -36,12 +46,18 @@ document.addEventListener('DOMContentLoaded', () => {
       event.target.dispatchEvent(new Event('change'));
     }
   };
+
   themeToggle.addEventListener('change', handleThemeToggleChange);
   themeToggle.addEventListener('keydown', handleThemeToggleKeydown);
 });
+
 const copyEmail = () => {
   const email = "erik@enkel.design";
   navigator.clipboard.writeText(email)
-    .then(() => alert(`${email} kopierat till urklipp.`))
-    .catch(err => console.error('Error copying text:', err));
+    .then(() => {
+      alert(`${email} kopierat till urklipp.`);
+    })
+    .catch(err => {
+      console.error('Error copying text:', err);
+    });
 };
