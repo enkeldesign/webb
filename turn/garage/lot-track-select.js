780a0bbf84ac4faac56b439e96dd748dcf7434f5
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-07-23 00:42:44 +0200

    Load r52 track runtime

--- turn/garage/lot-track-select.js
+++ turn/garage/lot-track-select.js
@@ -1,5 +1,5 @@
 import { showTheLot as showOriginalLot } from './lot-r10.js?build=20260720-r25';
-import { chooseTrackBeforeLot } from '../tracks/track-manager.js?build=20260722-r51';
+import { chooseTrackBeforeLot } from '../tracks/track-manager.js?build=20260722-r52';
 
 export async function showTheLot(options = {}) {
   const trackId = await chooseTrackBeforeLot();
