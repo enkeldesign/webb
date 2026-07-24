7ed5473f7c549b48810915eaeeb97bcd15f1c4ea
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-07-24 19:38:44 +0200

    TURN: install stat legend around The Lot flow

--- turn/garage/lot-track-select.js
+++ turn/garage/lot-track-select.js
@@ -1,8 +1,15 @@
 import { showTheLot as showOriginalLot } from './lot-r10.js?build=20260720-r25';
+import { installLotStatLegend } from './lot-stat-legend.js?build=20260724-r59';
 import { chooseTrackBeforeLot } from '../tracks/track-manager.js?build=20260722-r52';
 
 export async function showTheLot(options = {}) {
   const trackId = await chooseTrackBeforeLot();
   if (!trackId) return null;
-  return showOriginalLot(options);
+
+  const removeStatLegend = installLotStatLegend();
+  try {
+    return await showOriginalLot(options);
+  } finally {
+    removeStatLegend();
+  }
 }
