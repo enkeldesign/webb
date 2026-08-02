6b7fa2e2631dac02ec460d628b667c7aa5a2ed0b
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-02 04:13:10 +0200

    Use Midnight City r10 wall placement

--- turn/tracks/registry.js
+++ turn/tracks/registry.js
@@ -8,7 +8,7 @@ import {
 import { installAirportWorld } from './airport-world-r52.js?build=20260722-r52';
 import { installCliffsideWorld } from './cliffside-world.js';
 import { installHarborWorld } from './harbor-world.js';
-import { installMidnightCityWorld } from './midnight-city-world-r9.js?build=20260802-r9';
+import { installMidnightCityWorld } from './midnight-city-world-r10.js?build=20260802-r10';
 import { isForgivingTrackSurface } from './airport-runoff.js?build=20260722-r52';
 
 const WORLD_INSTALLERS = Object.freeze({
