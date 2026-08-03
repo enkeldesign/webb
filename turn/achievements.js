40e7f76c9ffe98a3e101bcff98d0b0c906cb225d
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-03 20:41:07 +0200

    Add achievement entry module

---
+++ turn/achievements.js
@@ -0,0 +1,10 @@
+export {
+  ACHIEVEMENTS,
+  ONBOARDING_ACHIEVEMENT_IDS
+} from './achievements/catalog.js?revision=r144-achievements';
+export {
+  ACHIEVEMENT_STORAGE_KEY,
+  loadAchievementState,
+  normalizeAchievementState
+} from './achievements/store.js?revision=r144-achievements';
+export { installAchievements } from './achievements/runtime.js?revision=r144-achievements';
