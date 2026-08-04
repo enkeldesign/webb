8d65846f2085828881560ce41e1e5885150de145
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-04 18:23:48 +0200

    Define hidden achievement collection

---
+++ turn/achievements/secret-catalog.js
@@ -0,0 +1,29 @@
+export const SECRET_ACHIEVEMENTS = Object.freeze([
+  Object.freeze({
+    id: 'find-lilya',
+    category: 'exploration',
+    trophies: 25,
+    hidden: true,
+    title: 'FIND LILYA!',
+    description: 'Find the hidden Lilya portrait in Midnight City.',
+    icon: 'spectate'
+  }),
+  Object.freeze({
+    id: 'find-darvid',
+    category: 'exploration',
+    trophies: 25,
+    hidden: true,
+    title: 'FIND DARVID!',
+    description: 'Find Darvid’s hidden face in Harbor.',
+    icon: 'spectate'
+  }),
+  Object.freeze({
+    id: 'satans-sedan',
+    category: 'exploration',
+    trophies: 25,
+    hidden: true,
+    title: 'SATAN’S SEDAN',
+    description: 'Unlock the Super Sedan by setting the Sport Sedan spoiler to color code #666.',
+    icon: 'rival'
+  })
+]);
