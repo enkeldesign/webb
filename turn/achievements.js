5dbb09aa07512140e871138ed0b77975637b3e36
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-05 21:20:12 +0200

    Export expanded TURN achievements

--- turn/achievements.js
+++ turn/achievements.js
@@ -1,20 +1,28 @@
 export {
   ACHIEVEMENTS,
   ONBOARDING_ACHIEVEMENT_IDS
-} from './achievements/catalog.js?revision=r157-hidden-achievements';
+} from './achievements/catalog.js?revision=r166-bella-records';
 export {
   ACHIEVEMENT_STORAGE_KEY,
   loadAchievementState,
   normalizeAchievementState,
   totalAvailableTrophies
-} from './achievements/store.js?revision=r157-hidden-achievements';
+} from './achievements/store.js?revision=r166-bella-records';
 export {
   TIME_TRIALS,
   TIME_TRIAL_ACHIEVEMENT_IDS,
   TIME_TRIAL_MASTER_ID,
   completedAllTimeTrials,
   qualifyingTimeTrial
-} from './achievements/time-trials.js?revision=r153-trophy-road';
+} from './achievements/time-trials.js?revision=r166-bella-records';
+export {
+  CLEAN_LAP_TARGETS,
+  CHALLENGE_PROGRESS_STORAGE_KEY,
+  normalizeChallengeProgress,
+  qualifiesForArmyLap,
+  qualifiesForCleanLap,
+  installAchievementChallengeExpansion
+} from './achievements/challenge-expansion-r166.js?revision=r166-bella-records';
 export {
   TROPHY_ROAD_REWARDS,
   TROPHY_ROAD_MAX_THRESHOLD,
@@ -22,5 +30,5 @@ export {
   isVehicleUnlocked,
   isPaintUnlocked,
   prepareTrophyRoadProfile
-} from './progression/trophy-road.js?revision=r157-paint-monster';
-export { installAchievements } from './achievements/runtime.js?revision=r157-hidden-achievements';
+} from './progression/trophy-road.js?revision=r166-bella-records';
+export { installAchievements } from './achievements/runtime.js?revision=r166-bella-records';
