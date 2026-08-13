d8c108224de37f70d4868dda7112e4036bc3277f
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-13 19:41:46 +0200

    Start tie tone adapter

---
+++ turn/audio/music/tone-runtime-ties.js
@@ -0,0 +1,5 @@
+import { createToneRuntime as createBaseToneRuntime } from './tone-runtime.js?revision=r184-score-v2';
+
+export function createToneRuntime(options) {
+  return createBaseToneRuntime(options);
+}
