d9b24093d83bb3967dfe0aa870e8dbfd96c2d4f2
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-13 15:16:57 +0200

    Update harbor.js

--- turn/audio/music/harbor.js
+++ turn/audio/music/harbor.js
@@ -1,193 +1,123 @@
-/*
 import { bars, makeSection, makeSong } from './song-tools.js?revision=r165-track-songbook-v1';
+
 
 const TUNE = makeSection({
   name: 'tune',
+
   lead: bars(
-    'A5 - C6 - E6 C6 A5 - G5 - E5 - A5 C6 - E6',
-    'C6 - E6 - G6 E6 C6 - Bb5 - G5 - E5 G5 - C6',
-    'D6 - F6 - A6 F6 D6 - C6 - A5 - F5 A5 - D6',
-    'E6 - B5 - G#5 B5 - D6 - B5 - G#5 A5 - E6 -'
+    'B5 - D6 G6 - F#6 - E6 - D6 B5 - A5 - B5 -',
+    'E6 - B5 E6 - G6 - F#6 E6 B5 - A5 B5 - D6 -',
+    'C6 - E6 G6 - C7 - B6 G6 E6 - D6 - E6 - G6',
+    'D6 - F#6 A6 - D7 - C7 A6 F#6 - E6 - D6 - A6'
   ),
+
+  bass: bars(
+    'G1 - G1 - D2 - G2 - G1 - D2 - B1 - D2 -',
+    'E1 - E1 - B1 - E2 - E1 - B1 - G1 - B1 -',
+    'C2 - C2 - G2 - C3 - C2 - G2 - E2 - G2 -',
+    'D2 - D2 - A2 - D3 - D2 - A2 - F#2 - A2 -'
+  ),
+
+  arp: bars(
+    'G3 D4 B3 D4 G4 D4 B3 D4 G3 B3 D4 F#4 G4 F#4 D4 B3',
+    'E3 B3 G3 B3 E4 B3 G3 B3 E3 G3 B3 D4 E4 D4 B3 G3',
+    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 B4 C5 B4 G4 E4',
+    'D4 A4 F#4 A4 D5 A4 F#4 A4 D4 F#4 A4 C5 D5 C5 A4 F#4'
+  ),
+
+  drums: bars(
+    'KH H K H SH H KH H K H K H SH H K OH',
+    'KH H K H SH H K OH KH H K H SH H KS OH',
+    'KH H KH H SH H K H K H KH H SH H K OH',
+    'KS H K H SH H KH OH K H KS H SH H KSO OH'
+  )
+});
+
+
+const CHORUS = makeSection({
+  name: 'chorus',
+  leadVoice: 'flute',
+
+  lead: bars(
+    'G5 G5 B5 B5 D6 D6 G6 G6 F#6 F#6 D6 D6 B5 B5 D6 D6',
+    'E5 E5 G5 G5 B5 B5 E6 E6 D6 D6 B5 B5 G5 G5 B5 B5',
+    'C5 C5 E5 E5 G5 G5 C6 C6 B5 B5 G5 G5 E5 E5 G5 G5',
+    'D5 D5 F#5 F#5 A5 A5 D6 D6 E6 E6 F#6 F#6 A6 A6 B6 B6'
+  ),
+
+  bass: bars(
+    'G1 G1 G1 G1 D2 D2 D2 D2 G2 G2 G2 G2 D2 D2 D2 D2',
+    'E1 E1 E1 E1 B1 B1 B1 B1 E2 E2 E2 E2 B1 B1 B1 B1',
+    'C2 C2 C2 C2 G2 G2 G2 G2 C3 C3 C3 C3 G2 G2 G2 G2',
+    'D2 D2 D2 D2 A2 A2 A2 A2 D3 D3 D3 D3 A2 A2 B2 B2'
+  ),
+
+  arp: bars(
+    'G3 - D4 - B3 - D4 - G4 - D4 - B3 - F#4 -',
+    'E3 - B3 - G3 - B3 - E4 - B3 - G3 - D4 -',
+    'C4 - G4 - E4 - G4 - C5 - G4 - E4 - B4 -',
+    'D4 - A4 - F#4 - A4 - D5 - A4 - F#4 - B4 -'
+  ),
+
+  drums: bars(
+    'KH H K H SH H K H KH H K H SH H K OH',
+    'KH H KH H SH H K OH KH H K H SH H KS OH',
+    'KH H K H SH H KH H K H K H SH H K OH',
+    'KS H KS H SH H K OH KS H KS H S KS KSO OH'
+  )
+});
+
+
+const BRIDGE = makeSection({
+  name: 'bridge',
+
+  lead: bars(
+    'A5 - C6 E6 - A6 - G6 E6 C6 - B5 - C6 - E6',
+    'C6 - E6 G6 - C7 - B6 G6 E6 - D6 - E6 - G6',
+    'E6 - G6 B6 - E7 - D7 B6 G6 - F#6 - G6 - B6',
+    'F#6 - A6 D7 - C7 - A6 F#6 E6 - D6 - A6 - D7'
+  ),
+
   bass: bars(
     'A1 - - - E2 - - - A2 - - - E2 - - -',
     'C2 - - - G2 - - - C3 - - - G2 - - -',
-    'D2 - - - A2 - - - D3 - - - A2 - - -',
-    'E2 - - - B2 - - - E3 - - - B2 - - -'
+    'E1 - - - B1 - - - E2 - - - B1 - - -',
+    'D2 - A2 - C3 - F#2 - D3 - A2 - C3 - D3 -'
   ),
+
   arp: bars(
     'A3 E4 C4 E4 A4 E4 C4 E4 A3 C4 E4 G4 A4 G4 E4 C4',
-    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 Bb4 C5 Bb4 G4 E4',
-    'D4 A4 F4 A4 D5 A4 F4 A4 D4 F4 A4 C5 D5 C5 A4 F4',
-    'E4 B4 G#4 B4 E5 B4 G#4 B4 E4 G#4 B4 D5 E5 D5 B4 G#4'
+    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 B4 C5 B4 G4 E4',
+    'E3 B3 G3 B3 E4 B3 G3 B3 E3 G3 B3 D4 E4 D4 B3 G3',
+    'D4 F#4 A4 C5 D5 C5 A4 F#4 D4 A4 C5 D5 F#5 D5 C5 A4'
   ),
-  drums: bars(
-    'KH H H H SH H K H KH H KH H SH H K OH',
-    'KH H K H SH H KH H K H H H SH H K OH',
-    'KH H KH H SH H K H KH H K H SH H K OH',
-    'KS H K H SH H KH OH KS H K H S H KSO OH'
-  )
-});
 
-const CHORUS = makeSection({
-  name: 'chorus',
-  leadVoice: 'flute',
-  lead: bars(
-    'A4 A4 E5 E5 C5 C5 E5 E5 A5 A5 G5 G5 E5 E5 C5 C5',
-    'C5 C5 G5 G5 E5 E5 G5 G5 C6 C6 Bb5 Bb5 G5 G5 E5 E5',
-    'D5 D5 A5 A5 F5 F5 A5 A5 D6 D6 C6 C6 A5 A5 F5 F5',
-    'E5 E5 B4 B4 G#4 G#4 B4 B4 D5 D5 E5 E5 G#5 G#5 B5 B5'
-  ),
-  bass: bars(
-    'A1 A1 A1 A1 E2 E2 E2 E2 A2 A2 A2 A2 E2 E2 E2 E2',
-    'C2 C2 C2 C2 G2 G2 G2 G2 C3 C3 C3 C3 G2 G2 G2 G2',
-    'D2 D2 D2 D2 A2 A2 A2 A2 D3 D3 D3 D3 A2 A2 A2 A2',
-    'E2 E2 E2 E2 B2 B2 B2 B2 E3 E3 E3 E3 B2 B2 B2 B2'
-  ),
-  arp: bars(
-    'A3 - C4 - E4 - C4 - A4 - E4 - C4 - E4 -',
-    'C4 - E4 - G4 - E4 - C5 - G4 - E4 - G4 -',
-    'D4 - F4 - A4 - F4 - D5 - A4 - F4 - A4 -',
-    'E4 - G#4 - B4 - G#4 - D5 - B4 - G#4 - B4 -'
-  ),
-  drums: bars(
-    'KH H H H SH H K H KH H H H SH H K OH',
-    'KH H K H SH H KH OH K H K H SH H K OH',
-    'KH H KH H SH H K H KH H H H SH H K OH',
-    'KS H K H SH H KS OH K H KS H S H KSO OH'
-  )
-});
-
-const BRIDGE = makeSection({
-  name: 'bridge',
-  lead: bars(
-    'F5 - A5 C6 - F6 - E6 - C6 - A5 F5 - C6 -',
-    'G5 - C6 E6 - G6 - E6 - D6 C6 - G5 - E6 -',
-    'B5 - D6 F6 - B6 - F6 - D6 B5 - A5 - F6 -',
-    'E6 D6 B5 - G#5 - E5 G#5 B5 - D6 - E6 - B5 -'
-  ),
-  bass: bars(
-    'F1 - - - C2 - - - F2 - - - C2 - - -',
-    'C2 - - - G2 - - - C3 - - - G2 - - -',
-    'B1 - - - F2 - - - B2 - - - F2 - - -',
-    'E2 - B2 - D3 - G#2 - E3 - B2 - D3 - B2 -'
-  ),
-  arp: bars(
-    'F4 A4 C5 A4 F5 C5 A4 C5 F4 A4 C5 E5 F5 E5 C5 A4',
-    'C4 E4 G4 E4 C5 G4 E4 G4 C4 G4 C5 E5 G5 E5 C5 G4',
-    'B3 D4 F4 D4 B4 F4 D4 F4 B3 D4 F4 A4 B4 A4 F4 D4',
-    'E4 G#4 B4 D5 E5 D5 B4 G#4 E4 B4 D5 E5 G#5 E5 D5 B4'
-  ),
-  drums: bars(
-    'K H - H S H K H KH H - H S H K OH',
-    'K H KH H S H K OH K H KH H S H K OH',
-    'KH H K H SH H KH H K H KH H SH H K OH',
-    'KS H KS H SH H K OH KS H K H S KS KSO OH'
-  )
-});
-
-export const HARBOR_SONG = makeSong({
-  id: 'harbor',
-  name: 'Dockside Rush',
-  bpm: 120,
-  sections: [TUNE, CHORUS, BRIDGE],
-  arrangement: ['bridge', 'tune', 'chorus', 'tune', 'chorus', 'bridge']
-});
-*/
-
-// Claude version
-import { bars, makeSection, makeSong } from './song-tools.js?revision=r165-track-songbook-v1';
-
-const TUNE = makeSection({
-  name: 'tune',
-  lead: bars(
-    'A5 - C6 E6 - C6 A5 - E5 G5 A5 - C6 A5 E5 -',
-    'F5 - A5 C6 - A5 F5 - C5 E5 F5 - A5 F5 C5 -',
-    'D6 - F6 A6 - F6 D6 - A5 C6 D6 - F6 D6 A5 -',
-    'E6 - G#5 B5 - G#5 E5 - D6 - B5 G#5 - E5 G#5 -'
-  ),
-  bass: bars(
-    'A1 - E2 - A2 - E2 - A1 - E2 - A2 - E2 -',
-    'F1 - C2 - F2 - C2 - F1 - C2 - F2 - C2 -',
-    'D2 - A2 - D3 - A2 - D2 - A2 - D3 - A2 -',
-    'E2 - B2 - E3 - B2 - E2 - B2 - E3 - B2 -'
-  ),
-  arp: bars(
-    'A3 C4 E4 A4 C5 E5 A5 E5 C5 A4 E4 C4 A3 C4 E4 A4',
-    'F3 A3 C4 F4 A4 C5 F5 C5 A4 F4 C4 A3 F3 A3 C4 F4',
-    'D4 F4 A4 D5 F5 A5 D6 A5 F5 D5 A4 F4 D4 F4 A4 D5',
-    'E4 G#4 B4 E5 G#5 B5 E6 B5 G#5 E5 B4 G#4 E4 G#4 B4 E5'
-  ),
-  drums: bars(
-    'KH H H KH SH H H H KH H KH H SH H K OH',
-    'KH H H KH SH H H H K H KH H SH H K OH',
-    'KH H H H SH H H H KH H K H SH H K OH',
-    'KS H K H SH H K OH KS H K H SH H KSO OH'
-  )
-});
-
-const CHORUS = makeSection({
-  name: 'chorus',
-  leadVoice: 'flute',
-  lead: bars(
-    'A4 A4 C5 C5 E5 E5 A5 A5 E5 E5 C5 C5 E5 E5 A4 A4',
-    'F4 F4 A4 A4 C5 C5 F5 F5 C5 C5 A4 A4 C5 C5 F4 F4',
-    'D5 D5 F5 F5 A5 A5 D6 D6 A5 A5 F5 F5 A5 A5 D5 D5',
-    'E5 E5 G#5 G#5 B5 B5 E6 E6 D6 D6 B5 B5 G#5 G#5 E5 E5'
-  ),
-  bass: bars(
-    'A1 A1 A1 A1 E2 E2 E2 E2 A2 A2 A2 A2 E2 E2 E2 E2',
-    'F1 F1 F1 F1 C2 C2 C2 C2 F2 F2 F2 F2 C2 C2 C2 C2',
-    'D2 D2 D2 D2 A2 A2 A2 A2 D3 D3 D3 D3 A2 A2 A2 A2',
-    'E2 E2 E2 E2 B2 B2 B2 B2 E3 E3 E3 E3 B2 B2 B2 B2'
-  ),
-  arp: bars(
-    'A3 - C4 - E4 - A4 - C5 - E4 - C4 - A3 -',
-    'F3 - A3 - C4 - F4 - A4 - C4 - A3 - F3 -',
-    'D4 - F4 - A4 - D5 - F5 - A4 - F4 - D4 -',
-    'E4 - G#4 - B4 - E5 - G#5 - B4 - G#4 - E4 -'
-  ),
-  drums: bars(
-    'KH H H H SH H KH H KH H H H SH H K OH',
-    'KH H H H SH H KH H K H H H SH H K OH',
-    'KH H H H SH H K H KH H H H SH H K OH',
-    'KS H K H SH H KH OH KS H K H SH H KSO OH'
-  )
-});
-
-const BRIDGE = makeSection({
-  name: 'bridge',
-  lead: bars(
-    'F5 - A5 C6 - F6 - C6 - A5 - F5 C6 - A5 -',
-    'C5 - E5 G5 - C6 - G5 - E5 - C5 G5 - E5 -',
-    'B5 - D6 F6 - B6 - F6 - D6 - A5 F6 - D6 -',
-    'E5 - G#5 B5 - E6 - D6 - B5 - G#5 - E6 - G#5'
-  ),
-  bass: bars(
-    'F1 - - - C2 - - - F2 - - - C2 - - -',
-    'C2 - - - G2 - - - C3 - - - G2 - - -',
-    'B1 - - - F2 - - - B2 - - - F2 - - -',
-    'E2 - G#2 - B2 - E3 - D3 - B2 - G#2 - E2 -'
-  ),
-  arp: bars(
-    'F3 A3 C4 F4 A4 C5 F5 A5 F5 C5 A4 F4 C4 A3 F3 C4',
-    'C4 E4 G4 C5 E5 G5 C6 E6 C6 G5 E5 C5 G4 E4 C4 G4',
-    'B3 D4 F4 B4 D5 F5 B5 F5 D5 B4 F4 D4 B3 D4 F4 A4',
-    'E4 G#4 B4 E5 G#5 B5 E6 B5 G#5 E5 D5 B4 G#4 E4 G#4 B4'
-  ),
   drums: bars(
     'K H - H S H - H K H - H S H K OH',
-    'K H - H S H K H K H - H S H K OH',
-    'KH H K H SH H K H KH H K H SH H K OH',
-    'KS H K H SH H K OH KS H K H S KS KSO OH'
+    'K H KH H S H K OH K H - H S H K OH',
+    'KH H - H SH H KH H K H - H SH H K OH',
+    'KS H KS H SH H K OH KS H KS H S KS KSO OH'
   )
 });
 
+
 export const HARBOR_SONG = makeSong({
   id: 'harbor',
-  name: 'Dockside Rush',
-  bpm: 120,
-  sections: [TUNE, CHORUS, BRIDGE],
-  arrangement: ['bridge', 'tune', 'chorus', 'tune', 'chorus', 'bridge']
-});
+  name: 'Harbor Run',
+  bpm: 128,
 
+  sections: [
+    TUNE,
+    CHORUS,
+    BRIDGE
+  ],
+
+  arrangement: [
+    'tune',
+    'chorus',
+    'bridge',
+    'tune',
+    'chorus',
+    'bridge'
+  ]
+});
