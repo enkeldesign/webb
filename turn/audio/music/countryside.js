69eb498344098df273f50aa1aa7693a1b7a12c40
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-14 09:49:50 +0200

    Update countryside.js

--- turn/audio/music/countryside.js
+++ turn/audio/music/countryside.js
@@ -2,7 +2,7 @@ import { bars, makeSection, makeSong } from './song-tools.js?revision=r186-note-
 
 const TUNE = makeSection({
   name: 'tune',
-  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'brass', bassVoice: 'warm', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
     'E5 - G5 B5 D6 - B5 G5 E5 - G5 A5 B5 D6 E6 -',
     'D6 - B5 A5 G5 - E5 G5 A5 B5 D6 B5 A5 G5 F#5 -',
@@ -10,10 +10,10 @@ const TUNE = makeSection({
     'B5 D6 E6 G6 F#6 E6 D6 B5 A5 B5 G5 F#5 E5 B5 E6 -'
   ),
   bass: bars(
-    'E2 - E2 E3 - B2 D3 - E2 - G2 B2 D3 B2 E3 -',
-    'C2 - C3 G2 - C3 B2 - D2 - D3 A2 D3 F#3 A2 -',
-    'E2 - E3 B2 D3 B2 G2 - E2 B2 E3 - D3 B2 G2 -',
-    'C2 - G2 C3 B2 - D3 C3 D2 A2 D3 F#3 E2 B2 E3 -'
+    'E3 - E3 E4 - B3 D4 - E3 - G3 B3 D4 B3 E4 -',
+    'C3 - C4 G3 - C4 B3 - D3 - D4 A3 D4 F#4 A3 -',
+    'E3 - E4 B3 D4 B3 G3 - E3 B3 E4 - D4 B3 G3 -',
+    'C3 - G3 C4 B3 - D4 C4 D3 A3 D4 F#4 E3 B3 E4 -'
   ),
   arp: bars(
     'E4 B4 G4 B4 E5 B4 G4 B4 E4 B4 G4 B4 E5 B4 G4 D5',
@@ -31,7 +31,7 @@ const TUNE = makeSection({
 
 const BRIDGE = makeSection({
   name: 'bridge',
-  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'brass', bassVoice: 'warm', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
     'G5 - B5 D6 G6 F#6 D6 B5 A5 B5 D6 E6 D6 B5 G5 -',
     'F#5 - A5 D6 F#6 E6 D6 A5 B5 D6 E6 F#6 E6 D6 A5 -',
@@ -39,10 +39,10 @@ const BRIDGE = makeSection({
     'F#5 A5 B5 D#6 F#6 D#6 B5 A5 F#5 A5 B5 D#6 F#6 D#6 B5 D#6'
   ),
   bass: bars(
-    'G2 - G2 D3 - G3 F#3 D3 G2 B2 D3 - G3 D3 B2 -',
-    'D2 - D3 A2 - D3 F#3 A2 D2 A2 D3 - F#3 D3 A2 -',
-    'C2 - C3 G2 - C3 E3 G2 C2 G2 C3 - E3 C3 G2 -',
-    'B1 - B2 F#2 A2 B2 D#3 F#3 B1 F#2 A2 B2 D#3 F#3 B2 D#3'
+    'G3 - G3 D4 - G4 F#4 D4 G3 B3 D4 - G4 D4 B3 -',
+    'D3 - D4 A3 - D4 F#4 A3 D3 A3 D4 - F#4 D4 A3 -',
+    'C3 - C4 G3 - C4 E4 G3 C3 G3 C4 - E4 C4 G3 -',
+    'B2 - B3 F#3 A3 B3 D#4 F#4 B2 F#3 A3 B3 D#4 F#4 B3 D#4'
   ),
   arp: bars(
     'G4 D5 B4 D5 G5 D5 B4 D5 G4 B4 D5 G5 D5 B4 D5 G5',
@@ -60,7 +60,7 @@ const BRIDGE = makeSection({
 
 const CHORUS = makeSection({
   name: 'chorus',
-  leadVoice: 'whistle', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'whistle', bassVoice: 'warm', arpVoice: 'mandolin', drumKit: 'brush',
   lead: bars(
     'E5 E5 G5 G5 B5 B5 G5 G5 E6 E6 B5 B5 G5 G5 B5 B5',
     'G5 G5 E5 E5 C6 C6 E6 E6 G6 G6 E6 E6 D6 D6 C6 C6',
