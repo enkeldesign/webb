111e634197b619403a5d362f64ad13a5d5ab1f79
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-14 02:12:39 +0200

    Update airport.js

--- turn/audio/music/airport.js
+++ turn/audio/music/airport.js
@@ -1,94 +1,94 @@
-import { bars, makeSection, makeSong } from './song-tools.js?revision=r184-score-v2';
+import { bars, makeSection, makeSong } from './song-tools.js?revision=r186-note-ties';
 
 const TUNE = makeSection({
-  name: 'tune', harmony: ['Am7', 'G7', 'G7', 'C'],
-  leadVoice: 'pulse', bassVoice: 'sub', arpVoice: 'glass', drumKit: 'electro',
+  name: 'tune',
+  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
-    'A5 - C6 E6 A6 - E6 C6 - A5 C6 - E6 - A6 -',
-    'G5 - B5 D6 F6 - D6 B5 - G5 B5 - D6 - G6 -',
-    'B5 - D6 F6 G6 - F6 D6 - B5 D6 - F6 - G6 -',
-    'C6 - E6 G6 C7 - G6 E6 - C6 E6 - G6 - C7 -'
+    'E5 - G5 B5 D6 - B5 G5 E5 - G5 A5 B5 D6 E6 -',
+    'D6 - B5 A5 G5 - E5 G5 A5 B5 D6 B5 A5 G5 F#5 -',
+    'E5 - B5 D6 E6 G6 F#6 E6 D6 - B5 G5 A5 B5 D6 -',
+    'B5 D6 E6 G6 F#6 E6 D6 B5 A5 B5 G5 F#5 E5 B5 E6 -'
   ),
   bass: bars(
-    'A1 - A2 - E2 - A2 - G2 - A2 - E2 - G2 -',
-    'G1 - G2 - D2 - G2 - F2 - G2 - D2 - F2 -',
-    'G1 - G2 - D2 - G2 - F2 - G2 - D2 - F2 -',
-    'C2 - C3 - G2 - C3 - E2 - C3 - G2 - E2 -'
+    'E2 - E2 E3 - B2 D3 - E2 - G2 B2 D3 B2 E3 -',
+    'C2 - C3 G2 - C3 B2 - D2 - D3 A2 D3 F#3 A2 -',
+    'E2 - E3 B2 D3 B2 G2 - E2 B2 E3 - D3 B2 G2 -',
+    'C2 - G2 C3 B2 - D3 C3 D2 A2 D3 F#3 E2 B2 E3 -'
   ),
   arp: bars(
-    'A4 C5 E5 G5 A5 G5 E5 C5 A4 C5 E5 G5 A5 G5 E5 C5',
-    'G4 B4 D5 F5 G5 F5 D5 B4 G4 B4 D5 F5 G5 F5 D5 B4',
-    'G4 B4 D5 F5 G5 F5 D5 B4 G4 B4 D5 F5 G5 F5 D5 B4',
-    'C5 E5 G5 E5 C6 G5 E5 G5 C5 E5 G5 C6 G5 E5 G5 E5'
+    'E4 B4 G4 B4 E5 B4 G4 B4 E4 B4 G4 B4 E5 B4 G4 D5',
+    'C4 G4 E4 G4 C5 G4 E4 G4 D4 A4 F#4 A4 D5 A4 F#4 A4',
+    'E4 B4 G4 B4 E5 B4 G4 B4 E4 G4 B4 D5 E5 D5 B4 G4',
+    'C4 G4 E4 G4 C5 B4 G4 E4 D4 A4 F#4 A4 E4 B4 G4 E5'
   ),
   drums: bars(
-    'KC H H H SC H H H K H H H SC H K OH',
-    'K H H H SC H K H KC H H H SC H K OH',
-    'KC H KH H SC H H H K H KH H SC H K OH',
-    'KC H KC H SC H K OH KC H KC H SC H KCO OH'
-  )
-});
-
-const CHORUS = makeSection({
-  name: 'chorus', harmony: ['C', 'Am7', 'G7', 'C'],
-  leadVoice: 'brass', bassVoice: 'sub', arpVoice: 'glass', drumKit: 'electro',
-  lead: bars(
-    'E6 E6 G6 - C7 C7 G6 - E6 G6 C7 - G6 - E6 -',
-    'E6 E6 A6 - C7 C7 A6 - E6 A6 C7 - A6 - E6 -',
-    'F6 F6 G6 - B6 B6 D7 - F7 D7 - B6 - G6 - D7',
-    'E6 E6 G6 - C7 C7 E7 - G7 E7 - C7 - G6 - C7'
-  ),
-  bass: bars(
-    'C2 C2 - G2 C3 C3 - E2 C2 C2 - G2 C3 C3 - E2',
-    'A1 A1 - E2 A2 A2 - G2 A1 A1 - E2 A2 A2 - G2',
-    'G1 G1 - D2 G2 G2 - F2 G1 G1 - D2 G2 G2 - F2',
-    'C2 C2 - G2 C3 C3 - E2 C2 C2 - G2 C3 C3 - E2'
-  ),
-  arp: bars(
-    'C5 - E5 G5 - E5 G5 C6 C5 - E5 G5 - E5 G5 -',
-    'A4 - C5 E5 - C5 E5 G5 A4 - C5 E5 - C5 E5 -',
-    'G4 - B4 D5 - F5 D5 B4 G4 - B4 D5 - F5 D5 -',
-    'C5 E5 G5 C6 G5 E5 G5 E5 C5 E5 G5 C6 G5 E5 G5 E5'
-  ),
-  drums: bars(
-    'KC H H H SC H K H KC H H H SC H K OH',
-    'KC H KH H SC H K OH KC H H H SC H KC OH',
-    'KC H H H SC H KH H K H KH H SC H K OH',
-    'KC H KC H SC H K OH KC H KC H SC H KCO OH'
+    'KH H H H SH H KH H KH H H H SH H K OH',
+    'KH H H H SH H K H KH H KH H SH H S OH',
+    'KH H H H SH H KH H KH H H H SH H K OH',
+    'KH H H H SH H KH H KH H SH H S S KS OH'
   )
 });
 
 const BRIDGE = makeSection({
-  name: 'bridge', harmony: ['C', 'Cm', 'G7', 'C'],
-  leadVoice: 'pulse', bassVoice: 'sub', arpVoice: 'glass', drumKit: 'electro',
+  name: 'bridge',
+  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
-    'C6 - E6 G6 - C7 - G6 E6 - D6 C6 - G6 - -',
-    'C6 - Eb6 G6 - C7 - G6 Eb6 - D6 C6 - G6 - -',
-    'B5 - D6 F6 G6 - F6 D6 - B5 - A5 G5 - D6 -',
-    'C6 E6 G6 - C7 - G6 E6 - D6 - C6 - G6 - C7'
+    'G5 - B5 D6 G6 F#6 D6 B5 A5 B5 D6 E6 D6 B5 G5 -',
+    'F#5 - A5 D6 F#6 E6 D6 A5 B5 D6 E6 F#6 E6 D6 A5 -',
+    'E5 G5 C6 E6 G6 E6 D6 C6 G5 C6 D6 E6 G6 E6 C6 -',
+    'F#5 A5 B5 D#6 F#6 D#6 B5 A5 F#5 A5 B5 D#6 F#6 D#6 B5 D#6'
   ),
   bass: bars(
-    'C2 - C3 - G2 - C3 - E2 - C3 - G2 - E2 -',
-    'C2 - C3 - G2 - C3 - Eb2 - C3 - G2 - Eb2 -',
-    'G1 - G2 - D2 - G2 - F2 - G2 - D2 - F2 -',
-    'C2 - C3 - G2 - C3 - E2 - C3 - G2 - E2 -'
+    'G2 - G2 D3 - G3 F#3 D3 G2 B2 D3 - G3 D3 B2 -',
+    'D2 - D3 A2 - D3 F#3 A2 D2 A2 D3 - F#3 D3 A2 -',
+    'C2 - C3 G2 - C3 E3 G2 C2 G2 C3 - E3 C3 G2 -',
+    'B1 - B2 F#2 A2 B2 D#3 F#3 B1 F#2 A2 B2 D#3 F#3 B2 D#3'
   ),
   arp: bars(
-    'C5 E5 G5 E5 C6 G5 E5 G5 C5 E5 G5 C6 G5 E5 G5 E5',
-    'C5 Eb5 G5 Eb5 C6 G5 Eb5 G5 C5 Eb5 G5 C6 G5 Eb5 G5 Eb5',
-    'G4 B4 D5 F5 G5 F5 D5 B4 G4 B4 D5 F5 G5 F5 D5 B4',
-    'C5 E5 G5 E5 C6 G5 E5 G5 C5 E5 G5 C6 G5 E5 G5 E5'
+    'G4 D5 B4 D5 G5 D5 B4 D5 G4 B4 D5 G5 D5 B4 D5 G5',
+    'D4 A4 F#4 A4 D5 A4 F#4 A4 D4 F#4 A4 D5 F#5 D5 A4 F#4',
+    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 C5 E5 C5 G4 E4',
+    'B3 F#4 A4 D#5 B4 F#4 A4 D#5 B3 A4 D#5 F#5 A5 F#5 D#5 B4'
   ),
   drums: bars(
-    'K H H H SC H K H K H H H SC H K OH',
-    'K H H H SC H K OH K H H H SC H KC OH',
-    'KC H KH H SC H K H KC H KH H SC H K OH',
-    'KC H KC H SC H K OH KC H KC H SC H KCO OH'
+    'KH H H H SH H KH H KH H KH H SH H K OH',
+    'KH H KH H SH H K H KH H KH H SH H KS OH',
+    'KH H H H SH H KH H KH H KH H SH H K OH',
+    'KH H KH H SH H KH H KS H KS H S KS KS KSO'
   )
 });
 
-export const AIRPORT_SONG = makeSong({
-  id: 'airport', name: 'Airport Runway', bpm: 144, key: 'C major / A minor',
-  style: 'high-energy runway electro pop', swing: 0,
-  sections: [TUNE, CHORUS, BRIDGE], arrangement: ['tune', 'chorus', 'tune', 'bridge', 'chorus', 'bridge']
+const CHORUS = makeSection({
+  name: 'chorus',
+  leadVoice: 'whistle', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  lead: bars(
+    'E5 E5 G5 G5 B5 B5 G5 G5 E6 E6 B5 B5 G5 G5 B5 B5',
+    'G5 G5 E5 E5 C6 C6 E6 E6 G6 G6 E6 E6 D6 D6 C6 C6',
+    'D6 D6 B5 B5 G5 G5 B5 B5 D6 D6 B5 B5 A5 A5 G5 G5',
+    'F#5 F#5 A5 A5 B5 B5 D#6 D#6 B5 B5 A5 A5 F#5 F#5 D#5 D#5'
+  ),
+  bass: bars(
+    'E2 E2 E2 E2 B2 B2 B2 B2 E3 E3 E3 E3 B2 B2 B2 B2',
+    'C2 C2 C2 C2 G2 G2 G2 G2 C3 C3 C3 C3 G2 G2 G2 G2',
+    'G2 G2 G2 G2 D3 D3 D3 D3 G3 G3 G3 G3 D3 D3 D3 D3',
+    'B1 B1 B1 B1 F#2 F#2 F#2 F#2 A2 A2 A2 A2 D#3 D#3 F#3 F#3'
+  ),
+  arp: bars(
+    'E4 - G4 - B4 - G4 - E5 - B4 - G4 - B4 -',
+    'C4 - E4 - G4 - E4 - C5 - G4 - E4 - G4 -',
+    'G4 - B4 - D5 - B4 - G5 - D5 - B4 - D5 -',
+    'B3 - D#4 - F#4 - A4 - B4 - F#4 - D#4 - F#4 -'
+  ),
+  drums: bars(
+    'KH H H H SH H - H KH H H H SH H K OH',
+    'KH H H H SH H - H KH H H OH SH H K OH',
+    'KH H H H SH H - H KH H H H SH H K OH',
+    'KH H H H SH H KH H KS H K OH SH H KS OH'
+  )
+});
+
+export const MENU_SONG = makeSong({
+  id: 'airport', name: 'TURN Theme', bpm: 128, key: 'E minor',
+  style: 'warm arcade title anthem', swing: 0.1,
+  sections: [TUNE, BRIDGE, CHORUS], arrangement: ['chorus', 'chorus', 'tune', 'tune', 'bridge', 'tune']
 });
