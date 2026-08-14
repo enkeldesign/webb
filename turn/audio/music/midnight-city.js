ec177f3baa98299720dae798b01aad20adc40fea
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-14 03:15:00 +0200

    Update midnight-city.js

--- turn/audio/music/midnight-city.js
+++ turn/audio/music/midnight-city.js
@@ -2,7 +2,7 @@ import { bars, makeSection, makeSong } from './song-tools.js?revision=r186-note-
 
 const TUNE = makeSection({
   name: 'tune',
-  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'bell', bassVoice: 'synth', arpVoice: 'glass', drumKit: 'electro',
   lead: bars(
     'E5 - G5 B5 D6 - B5 G5 E5 - G5 A5 B5 D6 E6 -',
     'D6 - B5 A5 G5 - E5 G5 A5 B5 D6 B5 A5 G5 F#5 -',
@@ -16,10 +16,10 @@ const TUNE = makeSection({
     'C2 - G2 C3 B2 - D3 C3 D2 A2 D3 F#3 E2 B2 E3 -'
   ),
   arp: bars(
-    'E4 B4 G4 B4 E5 B4 G4 B4 E4 B4 G4 B4 E5 B4 G4 D5',
-    'C4 G4 E4 G4 C5 G4 E4 G4 D4 A4 F#4 A4 D5 A4 F#4 A4',
-    'E4 B4 G4 B4 E5 B4 G4 B4 E4 G4 B4 D5 E5 D5 B4 G4',
-    'C4 G4 E4 G4 C5 B4 G4 E4 D4 A4 F#4 A4 E4 B4 G4 E5'
+    'E5 B5 G5 B5 E6 B5 G5 B5 E5 B5 G5 B5 E6 B5 G5 D6',
+    'C5 G5 E5 G5 C6 G5 E5 G5 D5 A5 F#5 A5 D6 A5 F#5 A5',
+    'E5 B5 G5 B5 E6 B5 G5 B5 E5 G5 B5 D6 E6 D6 B5 G5',
+    'C5 G5 E5 G5 C6 B5 G5 E5 D5 A5 F#5 A5 E5 B5 G5 E6'
   ),
   drums: bars(
     'KH H H H SH H KH H KH H H H SH H K OH',
@@ -31,7 +31,7 @@ const TUNE = makeSection({
 
 const BRIDGE = makeSection({
   name: 'bridge',
-  leadVoice: 'lead', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'bell', bassVoice: 'synth', arpVoice: 'glass', drumKit: 'electro',
   lead: bars(
     'G5 - B5 D6 G6 F#6 D6 B5 A5 B5 D6 E6 D6 B5 G5 -',
     'F#5 - A5 D6 F#6 E6 D6 A5 B5 D6 E6 F#6 E6 D6 A5 -',
@@ -45,10 +45,10 @@ const BRIDGE = makeSection({
     'B1 - B2 F#2 A2 B2 D#3 F#3 B1 F#2 A2 B2 D#3 F#3 B2 D#3'
   ),
   arp: bars(
-    'G4 D5 B4 D5 G5 D5 B4 D5 G4 B4 D5 G5 D5 B4 D5 G5',
-    'D4 A4 F#4 A4 D5 A4 F#4 A4 D4 F#4 A4 D5 F#5 D5 A4 F#4',
-    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 C5 E5 C5 G4 E4',
-    'B3 F#4 A4 D#5 B4 F#4 A4 D#5 B3 A4 D#5 F#5 A5 F#5 D#5 B4'
+    'G5 D6 B5 D6 G6 D6 B5 D6 G5 B5 D6 G6 D6 B5 D6 G6',
+    'D5 A5 F#5 A5 D6 A5 F#5 A5 D5 F#5 A5 D6 F#6 D6 A5 F#5',
+    'C5 G5 E5 G5 C6 G5 E5 G5 C5 E5 G5 C6 E6 C6 G5 E5',
+    'B4 F#5 A5 D#6 B5 F#5 A5 D#6 B4 A5 D#6 F#6 A6 F#6 D#6 B5'
   ),
   drums: bars(
     'KH H H H SH H KH H KH H KH H SH H K OH',
@@ -60,24 +60,24 @@ const BRIDGE = makeSection({
 
 const CHORUS = makeSection({
   name: 'chorus',
-  leadVoice: 'whistle', bassVoice: 'upright', arpVoice: 'organ', drumKit: 'brush',
+  leadVoice: 'neon', bassVoice: 'synth', arpVoice: 'glass', drumKit: 'electro',
   lead: bars(
-    'E5 E5 G5 G5 B5 B5 G5 G5 E6 E6 B5 B5 G5 G5 B5 B5',
-    'G5 G5 E5 E5 C6 C6 E6 E6 G6 G6 E6 E6 D6 D6 C6 C6',
-    'D6 D6 B5 B5 G5 G5 B5 B5 D6 D6 B5 B5 A5 A5 G5 G5',
-    'F#5 F#5 A5 A5 B5 B5 D#6 D#6 B5 B5 A5 A5 F#5 F#5 D#5 D#5'
+    'E4 E4 G4 G4 B4 B4 G4 G4 E5 E5 B4 B4 G4 G4 B4 B4',
+    'G4 G4 E4 E4 C5 C5 E5 E5 G5 G5 E5 E5 D5 D5 C5 C5',
+    'D5 D5 B4 B4 G4 G4 B4 B4 D5 D5 B4 B4 A4 A4 G4 G4',
+    'F#4 F#4 A4 A4 B4 B4 D#5 D#5 B4 B4 A4 A4 F#4 F#4 D#4 D#4'
   ),
   bass: bars(
-    'E2 E2 E2 E2 B2 B2 B2 B2 E3 E3 E3 E3 B2 B2 B2 B2',
-    'C2 C2 C2 C2 G2 G2 G2 G2 C3 C3 C3 C3 G2 G2 G2 G2',
-    'G2 G2 G2 G2 D3 D3 D3 D3 G3 G3 G3 G3 D3 D3 D3 D3',
-    'B1 B1 B1 B1 F#2 F#2 F#2 F#2 A2 A2 A2 A2 D#3 D#3 F#3 F#3'
+    'E3 E3 E3 E3 B3 B3 B3 B3 E4 E4 E4 E4 B3 B3 B3 B3',
+    'C3 C3 C3 C3 G3 G3 G3 G3 C4 C4 C4 C4 G3 G3 G3 G3',
+    'G3 G3 G3 G3 D4 D4 D4 D4 G4 G4 G4 G4 D4 D4 D4 D4',
+    'B2 B2 B2 B2 F#3 F#3 F#3 F#3 A3 A3 A3 A3 D#4 D#4 F#4 F#4'
   ),
   arp: bars(
-    'E4 - G4 - B4 - G4 - E5 - B4 - G4 - B4 -',
-    'C4 - E4 - G4 - E4 - C5 - G4 - E4 - G4 -',
-    'G4 - B4 - D5 - B4 - G5 - D5 - B4 - D5 -',
-    'B3 - D#4 - F#4 - A4 - B4 - F#4 - D#4 - F#4 -'
+    'E5 - G5 - B5 - G5 - E6 - B5 - G5 - B5 -',
+    'C5 - E5 - G5 - E5 - C6 - G5 - E5 - G5 -',
+    'G5 - B5 - D6 - B5 - G6 - D6 - B5 - D6 -',
+    'B4 - D#5 - F#5 - A5 - B5 - F#5 - D#5 - F#5 -'
   ),
   drums: bars(
     'KH H H H SH H - H KH H H H SH H K OH',
@@ -88,7 +88,7 @@ const CHORUS = makeSection({
 });
 
 export const MIDNIGHT_CITY_SONG = makeSong({
-  id: 'midnight-city', name: 'TURN Theme', bpm: 144, key: 'E minor',
+  id: 'midnight-city', name: 'TURN Theme', bpm: 128, key: 'E minor',
   style: 'warm arcade title anthem', swing: 0,
-  sections: [TUNE, BRIDGE, CHORUS], arrangement: ['bridge', 'bridge', 'chorus', 'chorus', 'tune', 'tune']
+  sections: [TUNE, BRIDGE, CHORUS], arrangement: ['tune', 'tune', 'bridge', 'tune', 'chorus', 'chorus']
 });
