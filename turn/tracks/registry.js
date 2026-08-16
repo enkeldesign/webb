9869eda58bdaf7bd9b3ff9f78e5cbd2737750577
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-16 17:30:37 +0200

    Cache-bust refined road edge styling

--- turn/tracks/registry.js
+++ turn/tracks/registry.js
@@ -11,7 +11,7 @@ import { installHarborWorld } from './harbor-world.js';
 // Historical regression markers: midnight-city-world-r9.js?build=20260802-r9, midnight-city-world-r10.js?build=20260802-r10
 import { installMidnightCityWorld } from './midnight-city-world-r11.js?build=20260802-r11';
 import { isForgivingTrackSurface } from './airport-runoff.js?build=20260722-r52';
-import './contextual-road-edges.js?revision=r507';
+import './contextual-road-edges.js?revision=r509';
 
 const WORLD_INSTALLERS = Object.freeze({
   countryside({ initialWorld }) {
