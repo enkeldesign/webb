3b382880ec7661513871b50cbe90ac1ac05c1d72
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-16 22:36:16 +0200

    Load start area visual polish

--- turn/tracks/registry.js
+++ turn/tracks/registry.js
@@ -13,6 +13,7 @@ import { installMidnightCityWorld } from './midnight-city-world-r11.js?build=202
 import { isForgivingTrackSurface } from './airport-runoff.js?build=20260722-r52';
 import './contextual-road-edges.js?revision=r518-signature-yellow';
 import './road-contour-color-r512.js?revision=r513-countryside';
+import './start-area-polish-r519.js?revision=r519-start-area-consistency';
 
 const WORLD_INSTALLERS = Object.freeze({
   countryside({ initialWorld }) {
