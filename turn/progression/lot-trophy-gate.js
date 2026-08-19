84575b7d7187e2b17be7f9c3a46283b35fdd55cb
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-19 13:34:09 +0200

    Use production Trophy Road order for vehicle gates

--- turn/progression/lot-trophy-gate.js
+++ turn/progression/lot-trophy-gate.js
@@ -3,7 +3,7 @@ import {
   isVehicleUnlocked,
   rewardForVehicle,
   showTrophyUnlockNotice
-} from './trophy-road.js?revision=r164-vintage-rally-perks';
+} from './trophy-road.js?revision=r166-bella-records';
 
 const FALLBACK_VEHICLE_ID = 'classic';
 const activeGates = new WeakMap();
