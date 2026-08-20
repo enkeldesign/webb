7357597f08e616695ce9f2a73d16ed94f4e10d1f
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-20 22:25:17 +0200

    Refresh Lot enhancement bundle

--- turn/garage/lot-track-select.js
+++ turn/garage/lot-track-select.js
@@ -1,7 +1,7 @@
 import {
   enhanceLotNow,
   prepareLotEnhancements
-} from './lot-enhancement-runtime.js?revision=r585-lot-redesign&build=20260804-r157';
+} from './lot-enhancement-runtime.js?revision=r587-rally-attributes&build=20260804-r157';
 import { chooseTrackBeforeLot } from '../tracks/track-manager.js?build=20260722-r52';
 import { showTrackIntro } from '../ui/track-intro.js?build=20260725-r75';
 
