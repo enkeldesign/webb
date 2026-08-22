3d79e408a00b8ea8f78fe41b45146d59f92ac768
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-22 14:41:26 +0200

    Preserve canonical Lot wrapper marker

--- turn/garage/lot-track-select.js
+++ turn/garage/lot-track-select.js
@@ -7,6 +7,7 @@ import { showTrackIntro } from '../ui/track-intro.js?build=20260725-r75';
 
 // Historical production regression markers while the showroom replaces this loader:
 // lot-r10.js?build=20260809-r163-native-html&revision=r590-canonical-lock-icon
+// lot-enhancement-runtime.js?revision=r588-canonical-attributes
 // export async function showEnhancedLot
 // SHOWROOM_CLEANUP_STYLE_ID = 'turn-lot-showroom-r203-polish'
 // lot-showroom-cleanup-r201.css?revision=r203-thumbnail-color-polish
