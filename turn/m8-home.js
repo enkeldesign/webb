e0baa69f2c3662402f667158153590811adc73ff
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-08-22 10:48:35 +0200

    Route M8 Home through showroom Lot

--- turn/m8-home.js
+++ turn/m8-home.js
@@ -6,7 +6,7 @@ import {
   normalizeTrackId
 } from '/turn/tracks/catalog.js?source=20260729-r118-m8';
 import { activateTrack } from '/turn/tracks/track-manager.js?source=20260729-r118-m8';
-import { showTheLot } from '/turn/garage/lot-r10.js?source=20260729-r118-m8';
+import { showEnhancedLot as showTheLot } from '/turn/garage/lot-track-select.js?revision=r200-production-candidate';
 import { showTrackIntro } from '/turn/ui/track-intro.js?source=20260729-r118-m8';
 import { getStoredBestLap } from '/turn/race/rival-storage.js?source=20260729-r118-m8';
 import { getCarDefinition } from '/turn/vehicle/catalog.js?source=20260729-r118-m8';
