e763a1d760a946797fd2c3ae26633ad55fb9c73f
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-10 22:57:50 +0200

    Make Open Horizon the Cliffside song

--- turn/audio/music/cliffside.js
+++ turn/audio/music/cliffside.js
@@ -1,94 +1,99 @@
 import { bars, makeSection, makeSong } from './song-tools.js?revision=r186-note-ties';
 
 const TUNE = makeSection({
-  name: 'tune',
-  leadVoice: 'reed', bassVoice: 'drive', arpVoice: 'organ', drumKit: 'brush',
+  name: 'tune', harmony: ['Bm7', 'G', 'D', 'A7'],
+  leadVoice: 'picked', bassVoice: 'warm', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
-    'E5 - G5 B5 D6 - B5 G5 E5 - G5 A5 B5 D6 E6 -',
-    'D6 - B5 A5 G5 - E5 G5 A5 B5 D6 B5 A5 G5 F#5 -',
-    'E5 - B5 D6 E6 G6 F#6 E6 D6 - B5 G5 A5 B5 D6 -',
-    'B5 D6 E6 G6 F#6 E6 D6 B5 A5 B5 G5 F#5 E5 B5 E6 -'
+    'B4 = D5 F#5 = A5 = F#5 E5 = D5 - F#5 = E5 -',
+    'G4 = B4 D5 = E5 = D5 B4 = A4 - B4 = G4 -',
+    'D5 = F#5 A5 = F#5 E5 D5 = C#5 B4 = A4 = F#4 -',
+    'E5 = F#5 A5 = G5 F#5 E5 = C#5 = B4 A4 = = -'
   ),
   bass: bars(
-    'E2 - E2 E3 - B2 D3 - E2 - G2 B2 D3 B2 E3 -',
-    'C2 - C3 G2 - C3 B2 - D2 - D3 A2 D3 F#3 A2 -',
-    'E2 - E3 B2 D3 B2 G2 - E2 B2 E3 - D3 B2 G2 -',
-    'C2 - G2 C3 B2 - D3 C3 D2 A2 D3 F#3 E2 B2 E3 -'
+    'B1 = F#2 - A2 = F#2 - D2 = F#2 - B2 = A2 -',
+    'G1 = D2 - B2 = G2 - D2 = G2 - B2 = D2 -',
+    'D2 = A2 - F#2 = A2 - D3 = A2 - F#2 = D2 -',
+    'A1 = E2 - G2 = E2 - C#2 = E2 - A2 = G2 -'
   ),
   arp: bars(
-    'E3 B3 G3 B3 E4 B3 G3 B3 E3 B3 G3 B3 E4 B3 G3 D4',
-    'C3 G3 E3 G3 C4 G3 E3 G3 D3 A3 F#3 A3 D4 A3 F#3 A3',
-    'E3 B3 G3 B3 E4 B3 G3 B3 E3 G3 B3 D4 E4 D4 B3 G3',
-    'C3 G3 E3 G3 C4 B3 G3 E3 D3 A3 F#3 A3 E3 B3 G3 E4'
+    'D4 - F#4 - A4 - F#4 - D5 - A4 - F#4 - E4 -',
+    'B3 - D4 - G4 - D4 - B4 - G4 - D4 - G3 -',
+    'F#3 - A3 - D4 - A3 - F#4 - D4 - A3 - F#3 -',
+    'G3 - A3 - C#4 - E4 - C#4 - A3 - G3 - E3 -'
   ),
   drums: bars(
-    'KH H H H SH H KH H KH H H H SH H K OH',
-    'KH H H H SH H K H KH H KH H SH H S OH',
-    'KH H H H SH H KH H KH H H H SH H K OH',
-    'KH H H H SH H KH H KH H SH H S S KS OH'
+    'K - H H S - H - K - H H S - O -',
+    'K - H H S - H - K H - H S - O -',
+    'K - H - S H H - K - H H S - O -',
+    'KH - H H S - H - K H H - S H O -'
   )
 });
 
 const BRIDGE = makeSection({
-  name: 'bridge',
-  leadVoice: 'reed', bassVoice: 'drive', arpVoice: 'organ', drumKit: 'brush',
+  name: 'bridge', harmony: ['Em7', 'C', 'Em7', 'A7'],
+  leadVoice: 'whistle', bassVoice: 'warm', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
-    'G5 - B5 D6 G6 F#6 D6 B5 A5 B5 D6 E6 D6 B5 G5 -',
-    'F#5 - A5 D6 F#6 E6 D6 A5 B5 D6 E6 F#6 E6 D6 A5 -',
-    'E5 G5 C6 E6 G6 E6 D6 C6 G5 C6 D6 E6 G6 E6 C6 -',
-    'F#5 A5 B5 D#6 F#6 D#6 B5 A5 F#5 A5 B5 D#6 F#6 D#6 B5 D#6'
+    'E5 = G5 B5 = A5 = G5 F#5 = E5 - D5 = E5 -',
+    'G5 = E5 D5 = C5 = E5 G5 = A5 - G5 = E5 -',
+    'E5 = G5 B5 = A5 = G5 F#5 = E5 - D5 = E5 -',
+    'C#5 = E5 A5 = G5 F#5 E5 = C#5 = B4 A4 = C#5 -'
   ),
   bass: bars(
-    'G2 - G2 D3 - G3 F#3 D3 G2 B2 D3 - G3 D3 B2 -',
-    'D2 - D3 A2 - D3 F#3 A2 D2 A2 D3 - F#3 D3 A2 -',
-    'C2 - C3 G2 - C3 E3 G2 C2 G2 C3 - E3 C3 G2 -',
-    'B1 - B2 F#2 A2 B2 D#3 F#3 B1 F#2 A2 B2 D#3 F#3 B2 D#3'
+    'E2 = B2 - D3 = B2 - G2 = B2 - E3 = D3 -',
+    'C2 = G2 - E3 = C3 - G2 = C3 - E3 = C3 -',
+    'E2 = B2 - D3 = B2 - G2 = B2 - E3 = D3 -',
+    'A1 = E2 - G2 = E2 - C#2 = E2 - A2 = G2 -'
   ),
   arp: bars(
-    'G4 D5 B4 D5 G5 D5 B4 D5 G4 B4 D5 G5 D5 B4 D5 G5',
-    'D4 A4 F#4 A4 D5 A4 F#4 A4 D4 F#4 A4 D5 F#5 D5 A4 F#4',
-    'C4 G4 E4 G4 C5 G4 E4 G4 C4 E4 G4 C5 E5 C5 G4 E4',
-    'B3 F#4 A4 D#5 B4 F#4 A4 D#5 B3 A4 D#5 F#5 A5 F#5 D#5 B4'
+    'G3 - B3 - D4 - B3 - G4 - D4 - B3 - G3 -',
+    'G3 - C4 - E4 - C4 - G4 - E4 - C4 - G3 -',
+    'G3 - B3 - D4 - B3 - G4 - D4 - B3 - G3 -',
+    'G3 - A3 - C#4 - E4 - C#4 - A3 - G3 - E3 -'
   ),
   drums: bars(
-    'KH H H H SH H KH H KH H KH H SH H K OH',
-    'KH H KH H SH H K H KH H KH H SH H KS OH',
-    'KH H H H SH H KH H KH H KH H SH H K OH',
-    'KH H KH H SH H KH H KS H KS H S KS KS KSO'
+    'K - H - S - H H K - H - S - O -',
+    'K - H - S - H - K - H H S - O -',
+    'K - H - S - H H K - H - S - O -',
+    'KH H - H S - H - K H H - S H O -'
   )
 });
 
 const CHORUS = makeSection({
-  name: 'chorus',
-  leadVoice: 'whistle', bassVoice: 'drive', arpVoice: 'glass', drumKit: 'brush',
+  name: 'chorus', harmony: ['G', 'D', 'Em7', 'A7'],
+  leadVoice: 'whistle', bassVoice: 'warm', arpVoice: 'organ', drumKit: 'brush',
   lead: bars(
-    'E5 E5 G5 G5 B5 B5 G5 G5 E6 E6 B5 B5 G5 G5 B5 B5',
-    'G5 G5 E5 E5 C6 C6 E6 E6 G6 G6 E6 E6 D6 D6 C6 C6',
-    'D6 D6 B5 B5 G5 G5 B5 B5 D6 D6 B5 B5 A5 A5 G5 G5',
-    'F#5 F#5 A5 A5 B5 B5 D#6 D#6 B5 B5 A5 A5 F#5 F#5 D#5 D#5'
+    'G5 = B5 D6 = B5 A5 G5 = F#5 = E5 D5 = G5 -',
+    'F#5 = A5 D6 = C#6 B5 A5 = F#5 = E5 D5 = A4 -',
+    'E5 = G5 B5 = A5 G5 F#5 = E5 = D5 B4 = E5 -',
+    'C#5 = E5 A5 = G5 F#5 E5 = C#5 = B4 A4 = C#5 -'
   ),
   bass: bars(
-    'E3 E3 E3 E3 B3 B3 B3 B3 E4 E4 E4 E4 B3 B3 B3 B3',
-    'C3 C3 C3 C3 G3 G3 G3 G3 C4 C4 C4 C4 G3 G3 G3 G3',
-    'G3 G3 G3 G3 D4 D4 D4 D4 G4 G4 G4 G4 D4 D4 D4 D4',
-    'B2 B2 B2 B2 F#3 F#3 F#3 F#3 A3 A3 A3 A3 D#4 D#4 F#4 F#4'
+    'G1 = D2 - B2 = G2 - D2 = G2 - B2 = D3 -',
+    'F#2 = A2 - D3 = A2 - F#2 = A2 - D3 = A2 -',
+    'E2 = B2 - D3 = B2 - G2 = B2 - E3 = D3 -',
+    'A1 = E2 - G2 = E2 - C#2 = E2 - A2 = G2 -'
   ),
   arp: bars(
-    'E5 - G5 - B5 - G5 - E6 - B5 - G5 - B5 -',
-    'C5 - E5 - G5 - E5 - C6 - G5 - E5 - G5 -',
-    'G5 - B5 - D6 - B5 - G6 - D6 - B5 - D6 -',
-    'B4 - D#5 - F#5 - A5 - B5 - F#5 - D#5 - F#5 -'
+    'B3 D4 G4 - D4 G4 B4 - G4 B4 D5 - B4 G4 D4 -',
+    'A3 D4 F#4 - D4 F#4 A4 - F#4 A4 D5 - A4 F#4 D4 -',
+    'G3 B3 D4 - B3 D4 G4 - D4 G4 B4 - G4 D4 B3 -',
+    'G3 A3 C#4 - A3 C#4 E4 - C#4 E4 G4 - E4 C#4 A3 -'
   ),
   drums: bars(
-    'KH H H H SH H - H KH H H H SH H K OH',
-    'KH H H H SH H - H KH H H OH SH H K OH',
-    'KH H H H SH H - H KH H H H SH H K OH',
-    'KH H H H SH H KH H KS H K OH SH H KS OH'
+    'KH H - H S H H - K H - H S H O -',
+    'KH H - H S - H H K H - H S H O -',
+    'KH H H - S H - H K H H - S H O -',
+    'KH H H - S H H - K H H H S H O -'
   )
 });
 
 export const CLIFFSIDE_SONG = makeSong({
-  id: 'cliffside', name: 'TURN Theme', bpm: 128, key: 'E minor',
-  style: 'warm arcade title anthem', swing: 0.12,
-  sections: [TUNE, BRIDGE, CHORUS], arrangement: ['tune', 'tune', 'bridge', 'tune', 'chorus', 'chorus']
+  id: 'cliffside',
+  name: 'Open Horizon',
+  bpm: 136,
+  key: 'B minor / D major',
+  style: 'flowing psychedelic pop with sweeping melodic lines, rolling bass and a strange cliff-edge harmonic turn',
+  swing: 0,
+  sections: [TUNE, BRIDGE, CHORUS],
+  arrangement: ['tune', 'tune', 'bridge', 'tune', 'chorus', 'chorus']
 });
