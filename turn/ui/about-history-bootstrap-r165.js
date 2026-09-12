9f689916412d6e8f561683a6fd90946c2b5f89c9
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-12 12:18:55 +0200

    Use current release history overlay

--- turn/ui/about-history-bootstrap-r165.js
+++ turn/ui/about-history-bootstrap-r165.js
@@ -2,7 +2,7 @@ import {
   CHANGELOG,
   CURRENT_RELEASE,
   DEVELOPMENT_HISTORY
-} from '../content/about-history.js?build=20260912-r217';
+} from '../content/about-history-current.js?build=20260912-r217';
 import { aboutTurnHtml } from '../content/about-turn.js?revision=r1';
 
 const REVISION = 'r165-browser-about';
