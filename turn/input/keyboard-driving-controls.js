ecf2723be18aa3d066f02f6806f3eeeda22bf38a
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-18 05:27:04 +0200

    Bind shared keyboard ownership to release identity

--- turn/input/keyboard-driving-controls.js
+++ turn/input/keyboard-driving-controls.js
@@ -1,7 +1,7 @@
 import {
   createKeyboardDriveOwnership,
   installKeyboardDriveOwnershipLossHandlers
-} from '/turn/input/keyboard-drive-ownership.js';
+} from './keyboard-drive-ownership.js?build=20260917-r254';
 
 const STEER_LEFT = 'steer-left';
 const STEER_RIGHT = 'steer-right';
