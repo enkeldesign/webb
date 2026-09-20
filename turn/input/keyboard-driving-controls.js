4179109e742400fa27361be2c9d79d1e8f238cd3
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 00:57:40 +0200

    TURN r271: advance release identity

--- turn/input/keyboard-driving-controls.js
+++ turn/input/keyboard-driving-controls.js
@@ -1,7 +1,7 @@
 import {
   createKeyboardDriveOwnership,
   installKeyboardDriveOwnershipLossHandlers
-} from './keyboard-drive-ownership.js?build=20260920-r270';
+} from './keyboard-drive-ownership.js?build=20260921-r271';
 
 const STEER_LEFT = 'steer-left';
 const STEER_RIGHT = 'steer-right';
