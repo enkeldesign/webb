6f87490af66ea6c6faa55f5f5e491c034109b9a6
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 00:13:45 +0200

    TURN 1.21.14: anchor night gradients to world horizon

--- turn/input/keyboard-driving-controls.js
+++ turn/input/keyboard-driving-controls.js
@@ -1,7 +1,7 @@
 import {
   createKeyboardDriveOwnership,
   installKeyboardDriveOwnershipLossHandlers
-} from './keyboard-drive-ownership.js?build=20260920-r268';
+} from './keyboard-drive-ownership.js?build=20260920-r269';
 
 const STEER_LEFT = 'steer-left';
 const STEER_RIGHT = 'steer-right';
--- turn/tracks/midnight-city-world-r2.js
+++ turn/tracks/midnight-city-world-r2.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { graphicsProfile } from '/turn/graphics-profile.js';
-import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260920-r268';
+import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260920-r269';
 
 const WARM_LIGHT = 0xffd27a;
 const PLAYER_FILL = 0xffe3b3;
