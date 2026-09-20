396fc2e2bea552d4696898266b9a239ce6e8fc77
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 00:57:50 +0200

    TURN r271: advance release identity

--- turn/tracks/midnight-city-world-r11.js
+++ turn/tracks/midnight-city-world-r11.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { signalSecretAchievement } from '../achievements/secret-events.js?revision=r157-hidden-achievements';
-import { installMidnightCityWorld as installMidnightCityWorldR7 } from './midnight-city-world-r7.js?build=20260920-r270';
+import { installMidnightCityWorld as installMidnightCityWorldR7 } from './midnight-city-world-r7.js?build=20260921-r271';
 import { installNightPlayerSpotlight } from './night-player-spotlight-r560.js?revision=r175-reconcile';
 import { installSharedNightSky } from '/turn/tracks/shared-night-sky.js';
 
