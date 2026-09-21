d6667f47a00b64e2c87b781dd50b9415b5cd72bd
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 10:58:59 +0200

    Advance turn/input/keyboard-driving-controls.js to TURN 1.21.26 r281

--- turn/input/keyboard-driving-controls.js
+++ turn/input/keyboard-driving-controls.js
@@ -1,7 +1,7 @@
 import {
   createKeyboardDriveOwnership,
   installKeyboardDriveOwnershipLossHandlers
-} from './keyboard-drive-ownership.js?build=20260921-r280';
+} from './keyboard-drive-ownership.js?build=20260921-r281';
 
 const STEER_LEFT = 'steer-left';
 const STEER_RIGHT = 'steer-right';
