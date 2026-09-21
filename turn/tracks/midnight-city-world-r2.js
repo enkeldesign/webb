f9817558d2312ce53782dd1a17bc27729ebe68d9
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 15:38:48 +0200

    Advance TURN r284 release references

--- turn/tracks/midnight-city-world-r2.js
+++ turn/tracks/midnight-city-world-r2.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { graphicsProfile } from '/turn/graphics-profile.js';
-import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260921-r283';
+import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260921-r284';
 
 const WARM_LIGHT = 0xffd27a;
 const PLAYER_FILL = 0xffe3b3;
