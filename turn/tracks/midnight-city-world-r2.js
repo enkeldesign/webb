0b176e105a0beec280430a1628c003e6035a2002
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 02:36:48 +0200

    TURN r276: advance release identity

--- turn/tracks/midnight-city-world-r2.js
+++ turn/tracks/midnight-city-world-r2.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { graphicsProfile } from '/turn/graphics-profile.js';
-import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260921-r275';
+import { installMidnightCityWorld as installMidnightCityWorldR1 } from './midnight-city-world.js?build=20260921-r276';
 
 const WARM_LIGHT = 0xffd27a;
 const PLAYER_FILL = 0xffe3b3;
