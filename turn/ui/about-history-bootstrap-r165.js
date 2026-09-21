fd8d792d89d4cfbcaf90e5167582d23e0a13c98b
enkeldesign <75029856+enkeldesign@users.noreply.github.com>
2026-09-21 12:05:14 +0200

    Advance turn/ui/about-history-bootstrap-r165.js to TURN 1.21.27 r282

--- turn/ui/about-history-bootstrap-r165.js
+++ turn/ui/about-history-bootstrap-r165.js
@@ -2,7 +2,7 @@ import {
   CHANGELOG,
   CURRENT_RELEASE,
   DEVELOPMENT_HISTORY
-} from '../content/about-history-current.js?build=20260921-r281';
+} from '../content/about-history-current.js?build=20260921-r282';
 import { aboutTurnHtml } from '../content/about-turn.js?revision=r1';
 
 const REVISION = 'r165-browser-about';
